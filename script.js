e30e2680696390875174b4235fb81df87269e9c1
Valetina-2608 <valusha2608@gmail.com>
2022-08-03 22:12:07 +0300

    Add redirect

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ loginBtn.addEventListener('click', loginUser);
 function loginUser(){
     let userEmail = document.getElementById('user_email').value;
     let userPassword = document.getElementById('user_password').value;
-
+    window.location.href = "homepage.html";
     let userData = {
      yourEmail: userEmail,
         yourPassword: userPassword
