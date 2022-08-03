39ddccd169334d332eaf904c0db037253428eb3b
Valetina-2608 <valusha2608@gmail.com>
2022-08-03 18:32:19 +0300

    Add function loginUser

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 /* Script */
+
+let loginBtn = document.getElementById('login_btn');
+loginBtn.addEventListener('click', loginUser);
+
+function loginUser(){
+    let userEmail = document.getElementById('user_email').value;
+    let userPassword = document.getElementById('user_password').value;
+
+    let userData = {
+     yourEmail: userEmail,
+        yourPassword: userPassword
+    }
+
+    localStorage.setItem('userLogin', JSON.stringify(userData));
+
+    function getData(){
+        let userGetData = localStorage.getItem('userLogin');
+    }
+
+}
